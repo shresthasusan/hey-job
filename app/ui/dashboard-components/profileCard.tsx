17622ebe107c6969bbc6eaf8ab93d3398ab1dfba
shresthasusan <shresthasusan44@gmail.com>
2024-08-09 09:07:50 +0545

    session sencond

--- app/ui/dashboard-components/profileCard.tsx
+++ app/ui/dashboard-components/profileCard.tsx
@@ -1,11 +1,15 @@
+'use client';
+
 import Image from "next/image";
 import React from "react";
-
+import { useSession } from "next-auth/react";
 interface Props {
   mode: string;
 }
 
 const ProfileCard = ({ mode }: Props) => {
+  const {data: session} = useSession();
+
   return (
     <>
       <div className="flex  min-w-[280px] w-[15%] flex-col relative rounded-3xl h-[250px] overflow-hidden shadow-[0_10px_20px_rgba(228,228,228,_0.7)] ">
@@ -34,7 +38,7 @@ const ProfileCard = ({ mode }: Props) => {
         <div className=" px-3  items-center justify-center h-2/3  flex flex-col">
           {/* <Image src="/" alt="profile" className="rounded-full" width={150} height={150} /> */}
           <div className=" text-center pt-10 ">
-            <h2 className="text-3xl  font-medium "> Rabin Yadav</h2>
+            <h2 className="text-3xl  font-medium "> {session?.user?.name}</h2>
             <p className="text-base  text-gray-400">{mode}</p>
           </div>
         </div>
