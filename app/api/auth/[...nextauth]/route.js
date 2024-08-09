566b514c3f9d4c5011ebda31a5bbb79c506fd89d
shresthasusan <shresthasusan44@gmail.com>
2024-08-09 22:38:19 +0545

    location error

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectMongoDB } from "../../../lib/mongodb";
 import bcrypt from "bcryptjs";
-import User from "@/models/User";
+import User from "../../../../models/User";
 
 export const authOptions = {
   providers: [
