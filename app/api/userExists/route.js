68673235243c059d0f7ce97109d5f57a8d86f77c
shresthasusan <shresthasusan44@gmail.com>
2024-08-09 09:14:43 +0545

    duplicate register debug

--- app/api/userExists/route.js
+++ app/api/userExists/route.js
@@ -8,9 +8,9 @@ export async function POST(req) {
     await connectMongoDB();
     const user = await User.findOne({ email }).select("_id");
     console.log(user); // This will log the user to thes
-    if (user) {
-      return NextResponse.json({ message: "User exists" }, { status: 200 });
-    }
+    // if (user) {
+    //   return NextResponse.json({ message: "User exists" }, { status: 200 });
+    // }
     return NextResponse.json({ user });
   } catch (error) {
     return NextResponse.json({ message: "Error" }, { status: 500 });
--- app/ui/dashboard-components/profileCard.tsx
+++ app/ui/dashboard-components/profileCard.tsx
@@ -38,7 +38,7 @@ const ProfileCard = ({ mode }: Props) => {
         <div className=" px-3  items-center justify-center h-2/3  flex flex-col">
           {/* <Image src="/" alt="profile" className="rounded-full" width={150} height={150} /> */}
           <div className=" text-center pt-10 ">
-            <h2 className="text-3xl  font-medium "> {session?.user?.name}</h2>
+            <h2 className="text-3xl  font-medium "> {session?.user?.name} </h2>
             <p className="text-base  text-gray-400">{mode}</p>
           </div>
         </div>
