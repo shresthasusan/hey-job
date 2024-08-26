73e88514caa00fa137c46889b03c5d4d5dc67086
shresthasusan <shresthasusan44@gmail.com>
2024-08-26 22:27:30 +0545

    backroll deployment error not resolved

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,18 +1,29 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import NextAuth from "next-auth";
+import { NextApiHandler } from "next";
 import { authOptions } from "@/app/lib/auth";
 
 // Type the handler properly for TypeScript
-export const config = {
-  runtime: "nodejs", // Explicitly use Node.js runtime
-};
-
-const handler = (req: NextApiRequest, res: NextApiResponse) =>
-  NextAuth(req, res, authOptions);
+const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
 
 // Export the handler for GET and POST requests
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
 
+//************************************ */
+// import { NextApiRequest, NextApiResponse } from "next";
+// import NextAuth from "next-auth";
+// import { authOptions } from "@/app/lib/auth";
+
+// // Type the handler properly for TypeScript
+// export const config = {
+//   runtime: "nodejs", // Explicitly use Node.js runtime
+// };
+
+// const handler = (req: NextApiRequest, res: NextApiResponse) =>
+//   NextAuth(req, res, authOptions);
+
+// // Export the handler for GET and POST requests
+// export { handler as GET, handler as POST };
+//************************************* */
 // import NextAuth from "next-auth";
 // import { NextApiHandler, NextApiResponse, NextApiRequest } from "next";
 // import { authOptions } from "@/app/lib/auth";
@@ -24,18 +35,4 @@ export { handler as GET, handler as POST };
 
 // // Export the handler for GET and POST requests
 // export { handler as GET, handler as POST };
-
-// import NextAuth, { AuthOptions, NextAuthOptions } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { connectMongoDB } from "../../../lib/mongodb";
-// import bcrypt from "bcryptjs";
-// import path from "path";
-// import { NextApiHandler } from "next";
-// import User from "../../../../models/user"; // Assuming this is a TypeScript file or has a declaration file
-// import { authOptions } from "@/app/lib/auth";
-
-// // Type the handler properly for TypeScript
-// const handler: NextApiHandler = (req, res) => NextAuth(req, res, authOptions);
-
-// // Export the handler for GET and POST requests
-// export { handler as GET, handler as POST, authOptions };
+//************************************ */
