63fdaec6540a2bb08c12778f399f1f5fb79f1982
shresthasusan <shresthasusan44@gmail.com>
2024-08-27 13:31:28 +0545

    test 1.2

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -3,7 +3,7 @@ import { NextApiHandler } from "next";
 import { authOptions } from "@/app/lib/auth";
 
 // Type the handler properly for TypeScript
-const handler: NextApiHandler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 // Export the handler for GET and POST requests
 export { handler as GET, handler as POST };
