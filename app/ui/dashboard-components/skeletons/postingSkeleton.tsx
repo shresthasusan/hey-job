72afd590bfb16d4bac5338b22afe7b6d26f496c0
shresthasusan <shresthasusan44@gmail.com>
2024-09-11 20:40:33 +0545

    refactor: Add margin top to PostingSkeleton component

--- app/ui/dashboard-components/skeletons/postingSkeleton.tsx
+++ app/ui/dashboard-components/skeletons/postingSkeleton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PostingSkeleton = () => {
   return (
     <>
-      <div className="w-full flex gap-10 flex-col  col-span-3">
+      <div className="w-full flex gap-10 flex-col mt-16 col-span-3">
         <div className="flex flex-col gap-4">
           <div className="h-3 bg-gray-200 rounded-full dark:bg-slate-200 w-48 mb-4"></div>
           <div className="h-3 bg-gray-200 rounded-full dark:bg-slate-200 max-w-[600px] mb-2.5"></div>
