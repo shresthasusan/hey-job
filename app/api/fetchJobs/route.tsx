677136d21d3037c0b207823e64f1e18d196371bf
shresthasusan <shresthasusan44@gmail.com>
2024-10-01 14:46:09 +0545

    Refactor fetchJobs route to fetch jobs excluding the logged-in user

--- app/api/fetchJobs/route.tsx
+++ app/api/fetchJobs/route.tsx
@@ -1,15 +1,26 @@
 import Jobs from "@/models/jobs";
 import { connectMongoDB } from "../../lib/mongodb";
 import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/lib/auth";
 
 export async function GET() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  }
+
   try {
     await connectMongoDB();
-    const jobs = await Jobs.find();
-    console.log("Jobs details fetched successfully");
+    const userId = session.user.id; // Get the logged-in user's ID from session
+
+    // Fetch all freelancers excluding the logged-in user
+    const jobs = await Jobs.find({ userId: { $ne: userId } });
+
     return NextResponse.json({ jobs });
   } catch (error) {
-    console.error("Error fetching jobs:", error);
+    console.error("Error fetching Jobs:", error);
     return NextResponse.json(
       { message: "Error fetching jobs" },
       { status: 500 }
