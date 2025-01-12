181823f88863ff61caba958d798c1d664c244eed
shresthasusan <shresthasusan44@gmail.com>
2025-01-12 21:05:32 +0545

    refactor: remove firebase.ts and update import path in firebaseStorage.ts

--- app/firebaseStorage.ts
+++ app/firebaseStorage.ts
@@ -1,5 +1,5 @@
 import { getStorage, ref, getDownloadURL } from "firebase/storage";
-import app from "./firebase";
+import app from "./lib/firebase";
 // Initialize storage
 const storage = getStorage(app);
 
