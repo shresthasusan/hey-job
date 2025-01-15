36e54bdf7e2e94f3e1a1a31fb936fea2cd45fa88
shresthasusan <shresthasusan44@gmail.com>
2025-01-15 10:10:05 +0545

    feat: enhance chat list component with user search functionality and chat management

--- app/ui/chat-component/chatWindow.tsx
+++ app/ui/chat-component/chatWindow.tsx
@@ -1,10 +1,135 @@
 "use client";
 
-import { Suspense } from "react";
+import { Suspense, useContext, useEffect, useState } from "react";
 import ChatList from "./chatList";
 import Image from "next/image";
+import { Appcontext } from "@/app/context/appContext";
+import { db, upload } from "@/app/lib/firebase";
+import {
+  updateDoc,
+  doc,
+  arrayUnion,
+  getDoc,
+  onSnapshot,
+} from "firebase/firestore";
+import {
+  PaperAirplaneIcon,
+  PhotoIcon,
+  ShareIcon,
+} from "@heroicons/react/24/outline";
 
 const ChatWindow: React.FC = () => {
+  const { userData, messagesId, chatUser, messages, setMessages, chatVisual } =
+    useContext(Appcontext) || {};
+  const [input, setInput] = useState("");
+
+  const sendMessage = async () => {
+    try {
+      if (input && messagesId && userData) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            text: input,
+            createdAt: new Date(),
+          }),
+        });
+        if (!chatUser) return;
+        const userIDs = [chatUser.rid, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c: { messageId: string }) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = input.slice(0, 30);
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.error((error as any).message);
+    }
+    setInput("");
+  };
+
+  const sendImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    try {
+      if (!userData) return;
+      if (!e.target.files) return;
+      const fileUrl = await upload(e.target.files[0]);
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+        const userIDs = [chatUser?.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c: { messageId: string }) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = "Image";
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const convertTimestamp = (timestamp: any) => {
+    let date = timestamp.toDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+
+    if (hour > 12) {
+      return hour - 12 + ":" + minute + "PM";
+    } else {
+      return hour + ":" + minute + "AM";
+    }
+  };
+
+  useEffect(() => {
+    if (messagesId) {
+      const unSub = onSnapshot(doc(db, "messages", messagesId), (res) => {
+        const data = res.data();
+
+        if (setMessages) {
+          setMessages(data?.messages.reverse());
+        }
+      });
+      return () => {
+        unSub();
+      };
+    }
+  }, [messagesId]);
+
   return (
     <>
       {/* Container for the entire chat window layout */}
@@ -19,101 +144,114 @@ const ChatWindow: React.FC = () => {
           </div>
 
           {/* Chat messages container */}
-          <div className="w-full rounded-3xl shadow-[0_10px_20px_rgba(228,228,228,_0.7)] px-5 flex flex-col justify-between">
-            {/* Messages section */}
-            <div className="flex flex-col mt-5">
-              {/* Example of a sent message */}
-              <div className="flex justify-end mb-4">
-                <div className="mr-2 py-3 px-4 bg-primary-500 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                  Welcome to group everyone!
+          {chatUser ? (
+            <>
+              <div className="w-full rounded-3xl shadow-[0_10px_20px_rgba(228,228,228,_0.7)] px-5 flex flex-col justify-between">
+                {/* Messages section */}
+                <div className="flex flex-col mt-5">
+                  {messages?.map(
+                    (
+                      msg: {
+                        sId: string;
+                        text?: string;
+                        image?: string;
+                        createdAt: any;
+                      },
+                      index: number
+                    ) => (
+                      <div key={index} className="flex justify-end mb-4">
+                        <div
+                          className={`py-3 px-4 rounded-tl-3xl rounded-tr-xl text-white rounded-bl-3xl ${
+                            msg.sId === userData?.id
+                              ? `bg-primary-500 mr-2`
+                              : `bg-gray-300 ml-2`
+                          }   `}
+                        >
+                          {msg["image"] ? (
+                            <Image
+                              width={200}
+                              height={200}
+                              src={msg.image}
+                              alt={"msg-image"}
+                            />
+                          ) : (
+                            <p className="msg">{msg.text}</p>
+                          )}
+                        </div>
+                        <Image
+                          src={
+                            msg.sId === userData?.id
+                              ? userData.avatar
+                              : chatUser?.userData.avatar
+                          }
+                          className="object-cover h-8 w-8 rounded-full"
+                          alt="User avatar"
+                          width={32}
+                          height={32}
+                        />
+                        <p>{convertTimestamp(msg.createdAt)}</p>
+                      </div>
+                    )
+                  )}
                 </div>
-                <Image
-                  src="/"
-                  className="object-cover h-8 w-8 rounded-full"
-                  alt="User avatar"
-                  width={32}
-                  height={32}
-                />
-              </div>
-
-              {/* Example of a received message */}
-              <div className="flex justify-start mb-4">
-                <Image
-                  src="/"
-                  className="object-cover h-8 w-8 rounded-full"
-                  alt="User avatar"
-                  width={32}
-                  height={32}
-                />
-                <div className="ml-2 py-3 px-4 bg-gray-300 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quaerat at praesentium, aut ullam delectus odio error sit rem.
+                {/* Input field for typing new messages */}
+                <div className="py-5 relative flex items-center">
+                  <input
+                    className="w-full bg-gray-200 py-5 px-3 rounded-xl"
+                    onChange={(e) => setInput(e.target.value)}
+                    value={input}
+                    type="text"
+                    placeholder="Type your message here..."
+                  />
+                  <input
+                    onChange={sendImage}
+                    type="file"
+                    id="image"
+                    accept="image/png, image/jpeg"
+                    hidden
+                  />
+                  <label htmlFor="image">
+                    <PhotoIcon className="w-8 h-8 top-0 " />
+                  </label>
+                  <span onClick={sendMessage}>
+                    <PaperAirplaneIcon className="w-8 h-8 top-0 " />
+                  </span>
+                  {/* hereee */}
                 </div>
               </div>
-
-              {/* Example of consecutive messages from the same user */}
-              <div className="flex justify-end mb-4">
-                <div>
-                  <div className="mr-2 py-3 px-4 bg-primary-500 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                    Lorem ipsum dolor sit amet.
+              {/* Group info section */}
+              <div className="w-2/5 border-l-2 px-5">
+                <div className="flex flex-col">
+                  {/* Group title */}
+                  <div className="font-semibold text-xl py-4">
+                    MERN Stack Group
                   </div>
-                  <div className="mt-4 mr-2 py-3 px-4 bg-primary-500 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-                    Lorem ipsum dolor sit amet consectetur.
+                  <Image
+                    src="/"
+                    className="object-cover rounded-xl h-64"
+                    alt="Group image"
+                    width={32}
+                    height={32}
+                  />
+                  <div className="font-semibold py-4">Created 22 Sep 2021</div>
+                  <div className="font-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Deserunt, perspiciatis!
                   </div>
                 </div>
-                <Image
-                  src="/"
-                  className="object-cover h-8 w-8 rounded-full"
-                  alt="User avatar"
-                  width={32}
-                  height={32}
-                />
               </div>
-
-              {/* Another example of a received message */}
-              <div className="flex justify-start mb-4">
-                <Image
-                  src="/"
-                  className="object-cover h-8 w-8 rounded-full"
-                  alt="User avatar"
-                  width={32}
-                  height={32}
-                />
-                <div className="ml-2 py-3 px-4 bg-gray-300 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
-                  Happy holiday, guys!
-                </div>
-              </div>
-            </div>
-
-            {/* Input field for typing new messages */}
-            <div className="py-5">
-              <input
-                className="w-full bg-gray-300 py-5 px-3 rounded-xl"
-                type="text"
-                placeholder="Type your message here..."
-              />
-            </div>
-          </div>
-
-          {/* Group info section */}
-          <div className="w-2/5 border-l-2 px-5">
-            <div className="flex flex-col">
-              {/* Group title */}
-              <div className="font-semibold text-xl py-4">MERN Stack Group</div>
+            </>
+          ) : (
+            <div className={`chat-welcome ${chatVisual ? "" : "hidden"}`}>
               <Image
-                src="/"
-                className="object-cover rounded-xl h-64"
-                alt="Group image"
+                src={"/logo/login-logo.png"}
                 width={32}
                 height={32}
+                alt={"logo"}
               />
-              <div className="font-semibold py-4">Created 22 Sep 2021</div>
-              <div className="font-light">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt, perspiciatis!
-              </div>
+              <p>chat anytime, anywhere</p>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </>
