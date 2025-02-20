4f426abeb92a3f10ba660cb41ab35bc456508aee
shresthasusan <shresthasusan44@gmail.com>
2025-02-20 13:56:32 +0545

    card component added

---
+++ app/ui/card.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface CardProps {
+  children: React.ReactNode; // Correct type for children
+  className: string;
+}
+
+const Card = ({ children, className }: CardProps) => {
+  // Destructure children from props
+  return (
+    <>
+      <div className={`w-full shadow-lg p-6 rounded-lg ${className}`}>
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default Card;
