65663cdd1be8f4064f12e298ae14b53423b5d069
shresthasusan <shresthasusan44@gmail.com>
2025-02-23 02:04:18 +0545

    fixed redirect

--- app/ui/kycStatus.tsx
+++ app/ui/kycStatus.tsx
@@ -18,32 +18,25 @@ const KYCStatus: React.FC = () => {
   const { data, error } = useFetch<KYCStatusResponse>(`/verification-status`); // fetch kycVerified
   const id = session?.user.id;
 
-  if (!session) {
-    router.push(`/login`);
-  }
-  if (session?.user.role !== "user") {
-    router.push("/");
-  }
+  useEffect(() => {
+    if (status !== "authenticated") return;
 
-  // useEffect(() => {
-  //   if (status !== "authenticated") return;
-
-  //   fetch("/api/user?fields=roles") // ✅ Fetch only roles from API
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       if (data.roles && !data.roles.client && !data.roles.freelancer) {
-  //         router.push(`/signup/profile-upload/${id}`);
-  //       }
-  //       if (pathname.startsWith("/user") && !data.roles.freelancer) {
-  //         console.log("false", data.roles.freelancer);
-  //         router.push(`/signup/freelancer`);
-  //       }
-  //       if (pathname.startsWith("/client") && !data.roles.client) {
-  //         router.push(`/signup/client`);
-  //       }
-  //     })
-  //     .catch((err) => console.error("Error fetching roles:", err));
-  // }, [status, id, router, pathname]);
+    fetch("/api/user?fields=roles") // ✅ Fetch only roles from API
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.roles && !data.roles.client && !data.roles.freelancer) {
+          router.push(`/signup/profile-upload/${id}`);
+        }
+        if (pathname.startsWith("/user") && !data.roles.freelancer) {
+          console.log("false", data.roles.freelancer);
+          router.push(`/signup/freelancer`);
+        }
+        if (pathname.startsWith("/client") && !data.roles.client) {
+          router.push(`/signup/client`);
+        }
+      })
+      .catch((err) => console.error("Error fetching roles:", err));
+  }, [status, id, router, pathname]);
 
   if (error) {
     console.error("Error fetching KYC status:", error);
