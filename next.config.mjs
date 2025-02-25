af65e099b90d4017268518a22cb3e9004cfe30c8
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-02-25 12:09:26 +0545

    Update redirect destination from user profile to best matches

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/user/profile",
+        destination: "/user/best-matches",
         permanent: true,
       },
     ];
