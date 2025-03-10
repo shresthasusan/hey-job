257e7357f04e2fad286abd73939733a854ec803b
shresthasusan <shresthasusan44@gmail.com>
2025-03-10 10:48:11 +0545

    Enhance proposal list and contract offers display; add loading state and expiration details, improve UI elements, and update offer filtering logic

--- app/user/(proposal&offers)/your-proposals/proposalList.tsx
+++ app/user/(proposal&offers)/your-proposals/proposalList.tsx
@@ -3,21 +3,20 @@ import useFetch from "@/app/hooks/useFetch";
 import { fetchWithAuth } from "@/app/lib/fetchWIthAuth";
 import { Button } from "@/app/ui/button";
 import Card from "@/app/ui/card";
+import CardSkeleton from "@/app/ui/dashboard-components/skeletons/cardSkeleton";
 import {
   CalculatorIcon,
-  CalendarDaysIcon,
   ClockIcon,
   CurrencyDollarIcon,
-  DocumentArrowUpIcon,
-  DocumentIcon,
+  ExclamationCircleIcon,
 } from "@heroicons/react/24/outline";
-import { DocumentTextIcon } from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 
 const ProposalList = () => {
   const [activeTab, setActiveTab] = useState("proposal-list");
   const { data: session } = useSession();
+  const [loading, setLoading] = useState(true);
 
   interface ContractOffer {
     id: string;
@@ -27,15 +26,35 @@ const ProposalList = () => {
     paymentType: string;
     deadline: string;
     isNew: boolean;
+    expiration: string;
+  }
+
+  interface proposal {
+    id: string;
+    title: string;
+    bidAmount: string;
+    status: string;
+    createdAt: string;
+    jobId: {
+      title: string;
+    };
   }
 
   const [contractOffers, setContractOffers] = useState<ContractOffer[]>([]);
+  const [proposal, setProposal] = useState<proposal[]>([]);
 
   useEffect(() => {
     const fetchContractOffers = async () => {
+      setLoading(true);
       const response = await fetchWithAuth(
         `/api/fetch-contracts?freelancerId=${session?.user.id}`
       );
+      const res = await fetchWithAuth(
+        `/api/jobproposal?freelancerId=${session?.user.id}`
+      );
+
+      const { proposals } = await res.json();
+      setProposal(proposals);
       const { data } = await response.json();
 
       // Map the response data to the ContractOffer structure
@@ -43,15 +62,17 @@ const ProposalList = () => {
         id: offer._id,
         title: offer.jobId?.title, // Extract title from jobId
         company: offer.clientDetails?.fullName, // Extract company name from clientDetails
-        price: offer.price.toString(), // Convert price to string if necessary
+        // Convert price to string if necessary
         paymentType: offer.paymentType,
         deadline: offer.deadline,
+        expiration: `expires in ${Math.ceil((new Date(offer.expiration).getTime() - Date.now()) / (1000 * 60 * 60 * 24))}`,
         isNew:
           new Date(offer.createdAt) >
-          new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), // Check if the offer is within the last 7 days
+          new Date(Date.now() - 3 * 24 * 60 * 60 * 1000), // Check if the offer is within the last 3 days
       }));
 
       setContractOffers(offers);
+      setLoading(false);
     };
 
     if (session?.user.id) {
@@ -78,44 +99,131 @@ const ProposalList = () => {
       {activeTab === "contract-offers" && (
         <div className="space-y-6">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {contractOffers.map((offer) => (
-              <Card key={offer.id} className="overflow-hidden">
-                <div className="pb-3">
-                  <div className="flex justify-between items-start">
-                    <div className="text-xl">{offer.title}</div>
-                    {offer.isNew && (
-                      <div className={`p-5 rounded-xl new-pin bg-primary-500`}>
-                        new
+            {loading
+              ? Array.from({ length: 6 }).map((_, index) => (
+                  <CardSkeleton key={index} />
+                ))
+              : contractOffers.map((offer) => (
+                  <Card key={offer.id} className="overflow-hidden">
+                    <div className="pb-3">
+                      <div className="flex justify-between items-start">
+                        <div className="text-2xl font-semibold">
+                          {offer.title}
+                        </div>
+                        {offer.isNew && (
+                          <div
+                            className={`p-1 rounded-lg new-pin text-xs text-white bg-primary-500`}
+                          ></div>
+                        )}
                       </div>
-                    )}
-                  </div>
-                  <div className="description">{offer.company}</div>
-                </div>
-                <div className="pb-3 content">
-                  <div className="space-y-3">
-                    <div className="flex items-center text-sm">
-                      <CurrencyDollarIcon className="h-4 w-4 mr-2 text-muted-foreground" />
-                      <span>{offer.price}</span>
+                      <div className="description mt-1 text-gray-500">
+                        {offer.company}
+                      </div>
                     </div>
-                    <div className="flex items-center text-sm">
-                      <CalculatorIcon className="h-4 w-4 mr-2 text-muted-foreground" />
-                      <span>{offer.paymentType}</span>
+                    <div className="pb-3 content">
+                      <div className="space-y-3">
+                        <div className="flex items-center text-sm">
+                          <CurrencyDollarIcon className="h-5 w-5 mr-2 text-gray-500" />
+                          <span>
+                            Offered Rate:{" "}
+                            {offer.paymentType === "hourly" ? "$/hr" : "$"}
+                            {offer.price}
+                          </span>
+                        </div>
+                        <div className="flex items-center text-sm">
+                          <CalculatorIcon className="h-5 w-5 mr-2 text-gray-500" />
+                          <span>{offer.paymentType}</span>
+                        </div>
+                        <div className="flex items-center text-sm">
+                          <ClockIcon className="h-5 w-5 mr-2 text-gray-500" />
+                          <span>
+                            Deadline:{" "}
+                            {new Date(offer.deadline).toLocaleDateString()}
+                          </span>
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex items-center text-sm">
-                      <ClockIcon className="h-4 w-4 mr-2 text-muted-foreground" />
-                      <span>
-                        Deadline:{" "}
-                        {new Date(offer.deadline).toLocaleDateString()}
-                      </span>
+                    <div className="footer flex justify-between pt-3">
+                      <div className="text-red-500 items-center flex py-2">
+                        <ExclamationCircleIcon className="h-5 w-5 mr-1" />
+                        {offer.expiration} days
+                      </div>
+                      <Button outline={true}>View Details</Button>
                     </div>
-                  </div>
-                </div>
-                <div className="footer flex justify-between pt-3">
-                  <Button>View Details</Button>
-                  <Button>Submit Proposal</Button>
-                </div>
-              </Card>
-            ))}
+                  </Card>
+                ))}
+          </div>
+        </div>
+      )}
+
+      {activeTab === "proposal-list" && (
+        <div className="space-y-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {loading
+              ? Array.from({ length: 6 }).map((_, index) => (
+                  <CardSkeleton key={index} />
+                ))
+              : proposal.map((p) => (
+                  <Card key={p.id} className="overflow-hidden">
+                    <div className="pb-3">
+                      <div className="flex justify-between items-start">
+                        <div className="text-2xl font-semibold">
+                          {p.jobId.title}
+                        </div>
+                        {p.status && (
+                          <div
+                            className={`px-3 p-1 rounded-lg new-pin text-xs text-white ${
+                              p.status === "pending"
+                                ? "bg-yellow-400"
+                                : p.status === "shortlisted"
+                                  ? "bg-blue-500"
+                                  : p.status === "accepted"
+                                    ? "bg-green-500"
+                                    : p.status === "rejected"
+                                      ? "bg-red-500"
+                                      : p.status === "withdrawn"
+                                        ? "bg-gray-500"
+                                        : p.status === "canceled"
+                                          ? "bg-black"
+                                          : ""
+                            }`}
+                          >
+                            {p.status}
+                          </div>
+                        )}
+                      </div>
+                      {/* <div className="description mt-1 text-gray-500">
+                      {p.company}
+                    </div> */}
+                    </div>
+                    <div className="pb-3 content">
+                      <div className="space-y-3">
+                        <div className="flex items-center text-sm">
+                          <CurrencyDollarIcon className="h-5 w-5 mr-2 text-gray-500" />
+                          <span>
+                            Your bid:{" "}
+                            {/* {p.paymentType === "hourly" ? "$/hr" : "$"} */}$
+                            {p.bidAmount}
+                          </span>
+                        </div>
+                        {/* <div className="flex items-center text-sm">
+                        <CalculatorIcon className="h-5 w-5 mr-2 text-gray-500" />
+                        <span>{p.paymentType}</span>
+                      </div> */}
+                        <div className="flex items-center text-sm">
+                          <ClockIcon className="h-5 w-5 mr-2 text-gray-500" />
+                          <span>
+                            submited:{" "}
+                            {new Date(p.createdAt).toLocaleDateString()}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="footer flex justify-between pt-3">
+                      <Button outline={true}>View Details</Button>
+                    </div>
+                  </Card>
+                ))}
           </div>
         </div>
       )}
