6c771555a6d029144b8a78275710592c6ae445cc
shresthasusan <shresthasusan44@gmail.com>
2025-03-12 11:44:22 +0545

    error solve

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,14 +1,12 @@
-'use client';
+"use client";
 
-import React from 'react';
-import DisplayProfile from '../../ui/user-component/displayProfile';
-
-
+import React from "react";
+import DisplayProfile from "@/app/ui/client-components/clientProfile";
 
 const UserProfile: React.FC = () => {
   return (
-    <div  >
-      <h1 className='font-black text-center text-5xl'>User Profile</h1>
+    <div>
+      <h1 className="font-black text-center text-5xl">User Profile</h1>
       <DisplayProfile />
     </div>
   );
