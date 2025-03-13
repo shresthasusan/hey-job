3bef93ad9a2a3a9578787e60a174b36c169f03a8
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-03-13 22:12:15 +0545

    Refactor SettingsPage component to use Suspense with a loading fallback

--- app/user/setting/page.tsx
+++ app/user/setting/page.tsx
@@ -1,28 +1,16 @@
-<<<<<<< HEAD
 import SettingsPage from '@/app/ui/user-component/usersettings'
 import React, { Suspense } from 'react'
-=======
-import SettingsPage from "@/app/ui/user-component/usersettings";
-import React, { Suspense } from "react";
->>>>>>> d4bf7741fdfc5cb468979a95ee8f5cc821a0c428
 
 const page = () => {
   return (
     <div>
-<<<<<<< HEAD
       <h1 className='text-4xl text-center mt-8 font-semibold'>Settings</h1>
     <Suspense>
     <SettingsPage />
     </Suspense>
       
-=======
-      <h1 className="text-4xl text-center mt-8 font-semibold">Settings</h1>
-      <Suspense fallback={<div>loading</div>}>
-        <SettingsPage />
-      </Suspense>
->>>>>>> d4bf7741fdfc5cb468979a95ee8f5cc821a0c428
     </div>
-  );
-};
+  )
+}
 
-export default page;
+export default page
