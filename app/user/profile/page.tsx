a2ba6437bb1bc7566ccfdba20b92165ccd4c8cd9
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-03-13 22:12:48 +0545

    Add loading fallback to Suspense in UserProfile and SettingsPage components

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -7,7 +7,7 @@ const UserProfile: React.FC = () => {
   return (
     <div>
       <h1 className="font-black text-center text-5xl">User Profile</h1>
-      <Suspense>
+      <Suspense fallback={<div>Loading...</div>}>
       <DisplayProfile />
       </Suspense>
     </div>
--- app/user/setting/page.tsx
+++ app/user/setting/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <div>
       <h1 className='text-4xl text-center mt-8 font-semibold'>Settings</h1>
-    <Suspense>
+    <Suspense fallback={<div>Loading...</div>}>
     <SettingsPage />
     </Suspense>
       
