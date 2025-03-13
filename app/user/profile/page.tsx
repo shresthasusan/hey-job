b91e8103b27c1cca099bdb5b07e72c7e6b094b3e
Sujit-Thapa <sujitthapa.professiona@gmaill.com>
2025-03-13 22:05:49 +0545

    Wrap SettingsPage component in Suspense for improved loading handling

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import React from "react";
+import React, { Suspense } from "react";
 import DisplayProfile from "@/app/ui/client-components/clientProfile";
 
 const UserProfile: React.FC = () => {
   return (
     <div>
       <h1 className="font-black text-center text-5xl">User Profile</h1>
+      <Suspense>
       <DisplayProfile />
+      </Suspense>
     </div>
   );
 };
--- app/user/setting/page.tsx
+++ app/user/setting/page.tsx
@@ -1,11 +1,13 @@
 import SettingsPage from '@/app/ui/user-component/usersettings'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
   return (
     <div>
       <h1 className='text-4xl text-center mt-8 font-semibold'>Settings</h1>
-      <SettingsPage />
+    <Suspense>
+    <SettingsPage />
+    </Suspense>
       
     </div>
   )
