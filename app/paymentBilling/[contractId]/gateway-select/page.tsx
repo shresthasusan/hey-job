feda4b218570afb08e0587f8965301e0e905a395
shresthasusan <shresthasusan44@gmail.com>
2025-03-16 14:48:36 +0545

    feat: refactor payment gateway page to include loading state and improve structure

--- app/paymentBilling/[contractId]/gateway-select/page.tsx
+++ app/paymentBilling/[contractId]/gateway-select/page.tsx
@@ -1,8 +1,19 @@
-import PaymentsPage from "@/app/user/payments/page";
-import React from "react";
+import React, { Suspense } from "react";
+import { Payment } from "@/app/ui/payment";
 
-const page = () => {
-  return <PaymentsPage />;
+const PaymentsPage: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold mt-5 text-center">
+        Payment Gateways
+      </h1>
+      <div className="flex justify-center mt-10">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Payment />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default PaymentsPage;
